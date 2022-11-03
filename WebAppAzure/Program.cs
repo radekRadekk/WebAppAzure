90d33d3485fba51f7921975f1c4656a2b0280efa
radekRadekk <radek@radek.com>
2022-11-03 10:37:58 +0100

    Initial commit

---
+++ WebAppAzure/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var builder = WebApplication.CreateBuilder();
+
+var app = builder.Build();
+
+app.MapGet("/test", () => "Hello Radek");
+
+app.Run();
